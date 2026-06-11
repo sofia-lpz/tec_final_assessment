768ac4b039704bdb3fda29d05c7206f9bfbe264d
sofia-lpz <sofialopezcontact@gmail.com>
2026-06-11 16:38:35 -0600

    Added login

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: '/api/:path*',
+          destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api'}/:path*`,
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
